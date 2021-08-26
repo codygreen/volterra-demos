4ea4f6a8e1f83da19264bdafed0b2be43d680650
Cody Green <cody@codygreen.com>
2021-08-26 12:52:23 -0500

    fix pre-commit issues

--- modules/aws/k3s/deploy_app.sh
+++ modules/aws/k3s/deploy_app.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 #Get k3s hostname
-export k3s_host=`terraform output -json | jq '.tcp_vs_host_name.value' -r`
-echo $k3s_host
-
-scp -i rsa.key ubuntu@$k3s_host:/etc/rancher/k3s/k3s.yaml .
+k3s_host=$(terraform output -json | jq '.tcp_vs_host_name.value' -r)
+scp -i rsa.key ubuntu@"$k3s_host":/etc/rancher/k3s/k3s.yaml .
